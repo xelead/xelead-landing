b7350663b564c6505bba8a22a0a24e6230b87983
mohghaderi <mohghaderi@gmail.com>
2026-02-17 10:42:56 -0600

    update config

--- src/landing_page/config.ts
+++ src/landing_page/config.ts
@@ -1,11 +1,13 @@
 type LandingPageConfig = {
 	turnstileSiteKey: string;
 	apiBaseUrl: string;
+	assetsUrl: string;
 };
 
 export const landingPageConfig: LandingPageConfig = {
 	turnstileSiteKey: process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY ?? "",
 	apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "",
+	assetsUrl: process.env.NEXT_PUBLIC_ASSETS_BASE_URL ?? ""
 };
 
 console.log("Configuration as:", landingPageConfig)
